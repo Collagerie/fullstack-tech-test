7be457b9423eb4df9672edf075990b899b2445df
kaustubh285 <kaustubh285@gmail.com>
2024-01-26 11:31:35 +0000

    fix: removal of unnecessay code

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,15 +3,8 @@ import mockData from "../../mockdata/allCharacters.json";
 import { ICharacterCore } from "@/types/types";
 import CharacterBlock from "./organisms/CharacterBlock";
 const HomePage = () => {
-  const x = [
-    1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 1, 2, 3,
-    4, 5, 6, 7, 8, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 1, 2, 3, 4, 5, 6,
-    7, 8, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 8,
-    9, 10,
-  ];
   const newData: ICharacterCore[] = [];
 
-  mockData.data.forEach((char) => newData.push(char));
   mockData.data.forEach((char) => newData.push(char));
   return (
     <div className='flex flex-col z-50 overflow-scroll w-screen pt-24 space-y-12 md:px-20 px-3'>
