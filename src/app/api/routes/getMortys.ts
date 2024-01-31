606b4f70d3bde235339a667be7214c25caa8d6b2
George Worsnop <georgeworsnop@gmail.com>
2024-01-31 12:04:25 +0000

    feat(api): format api call and add typing

--- src/app/api/routes/getMortys.ts
+++ src/app/api/routes/getMortys.ts
@@ -1,4 +1,6 @@
-const url = 'https://rickandmortyapi.com/graphql'
+import { ICharacter, IEpisode, ILocation } from "@/types/types";
+
+const url = "https://rickandmortyapi.com/graphql";
 
 const query = `
 query getMortys($name: String, $status: String, $page: Int){
@@ -31,63 +33,56 @@ query getMortys($name: String, $status: String, $page: Int){
       }
     }
   }
-`
+`;
 
-const getData = async (page = 1) => {
+export const getData = async ({ page = 1 }) => {
   try {
     const response = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         query: query,
-        variables: { name: "Morty", status: "Alive", page},
+        variables: { name: "Morty", status: "Alive", page },
       }),
     });
 
     const { data } = await response.json();
 
     const characterData = data.characters.results.map((character) => {
-
-      const episodeInfo = character.episode.map((episode) => {
+      const episodeInfo: IEpisode = character.episode.map((episode) => {
         return {
           id: episode.id,
           name: episode.name,
           airDate: episode.air_date,
           noOfCharacters: episode.characters.length,
           episode: episode.episode,
-        }
-      })
+        };
+      });
 
-      const locationInfo = {
-          id: character.location.id,
-          name: character.location.name,
-          type: character.location.type,
-          noOfResidents: character.location.residents.length,
-          dimension: character.location.dimension
-        }
-      
+      const locationInfo: ILocation = {
+        id: character.location.id,
+        name: character.location.name,
+        type: character.location.type,
+        noOfResidents: character.location.residents.length,
+        dimension: character.location.dimension,
+      };
+
       return {
-          id: character.id,
-          name: character.name,
-          status: character.status,
-          species: character.species,
-          gender: character.gender,
-          avatar: character.image,
-          episode: episodeInfo,
-          location: locationInfo
-      }
-    })
+        id: character.id,
+        name: character.name,
+        status: character.status,
+        species: character.species,
+        gender: character.gender,
+        avatar: character.image,
+        episodes: episodeInfo,
+        location: locationInfo,
+      };
+    });
 
-    console.log(JSON.stringify(characterData));
-    return characterData;
+    return characterData as ICharacter;
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
   }
 };
-
-(async () => {
-  await getData()
-  console.log('✅ Done')
-})()
