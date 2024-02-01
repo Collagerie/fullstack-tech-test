66938e5b4d221e8095671e0310fa1cfbba1a3794
George Worsnop <georgeworsnop@gmail.com>
2024-02-01 13:59:31 +0000

    feat(character): combine components in character page

---
+++ src/app/character/[id]/page.tsx
@@ -0,0 +1,11 @@
+import CharacterHeader from "@/components/molecules/CharacterHeader";
+import CharacterPage from "@/components/organisms/CharacterPage";
+
+export default async function Character() {
+  return (
+    <>
+      <CharacterHeader />
+      <CharacterPage />
+    </>
+  );
+}
